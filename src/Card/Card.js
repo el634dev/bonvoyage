77b35660e2def1d6a3c66dcdd969a7e8d8e7428a
el634dev <al7758066@gmail.com>
2024-02-27 10:54:56 -0500

    Completed hero and card sections

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -3,170 +3,174 @@ import React from 'react';
 
 function Card() {
     return (
-        <div>
-        
-<link rel="stylesheet" href="https://cdn.tailgrids.com/tailgrids-fallback.css" />
-
-<section class="pt-20 lg:pt-[120px] pb-10 lg:pb-20 bg-[#F3F4F6]">
-   <div class="container">
-      <div class="flex flex-wrap -mx-4">
-         <div class="w-full md:w-1/2 xl:w-1/3 px-4">
-            <div class="bg-white rounded-lg overflow-hidden mb-10">
-               <img
-                  src="https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dG9reW98ZW58MHx8MHx8fDA%3D"
-                  alt=""
-                  class="w-full"
-                  loading='lazy'
-                  />
-               <div class="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
-                  <h3>
-                     <a
-                        href="javascript:void(0)"
-                        class="
-                        font-semibold
-                        text-dark text-xl
-                        sm:text-[22px]
-                        md:text-xl
-                        lg:text-[22px]
-                        xl:text-xl
-                        2xl:text-[22px]
-                        mb-4
-                        block
-                        hover:text-primary
-                        "
-                        >
-                     Toyko
-                     </a>
-                  </h3>
-                  <p class="text-base text-body-color leading-relaxed mb-7">
-                    Tokyo is located in the center and heart of Japan, one of the most famous cities in the world. Know for futuristic locations.
-                  </p>
-                  <a
-                     href="#"
-                     class="
-                     inline-block
-                     py-2
-                     px-7
-                     border border-[#E5E7EB]
-                     rounded-full
-                     text-base text-body-color
-                     font-medium
-                     hover:border-primary hover:bg-primary hover:text-white
-                     transition
-                     "
-                     >
-                    Explore
-                  </a>
+      <div>
+         <link rel="stylesheet" href="https://cdn.tailgrids.com/tailgrids-fallback.css" />
+         <section class="pt-20 lg:pt-[120px] pb-10 lg:pb-20 bg-[#F3F4F6]">
+            <div class="container">
+               <div class="flex flex-wrap -mx-4">
+                  <div class="w-full md:w-1/2 xl:w-1/3 px-4">
+                     <div class="bg-white rounded-lg overflow-hidden mb-10">
+                        <img
+                           src="https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dG9reW98ZW58MHx8MHx8fDA%3D"
+                           alt=""
+                           class="w-full"
+                           loading='lazy'
+                           />
+                        <div class="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
+                        <h3>
+                           <a
+                              href="javascript:void(0)"
+                              class="
+                              font-semibold
+                              text-dark text-xl
+                              sm:text-[22px]
+                              md:text-xl
+                              lg:text-[22px]
+                              xl:text-xl
+                              2xl:text-[22px]
+                              mb-4
+                              block
+                              hover:text-primary
+                              "
+                              >
+                           Toyko
+                           </a>
+                        </h3>
+                        {/* Card 1 Description */}
+                        <p class="text-base text-body-color leading-relaxed mb-7">
+                        Tokyo is located in the center and heart of Japan, one of the most famous cities in the world. Know for futuristic locations.
+                        </p>
+                        <a
+                           href="#"
+                           class="
+                           inline-block
+                           py-2
+                           px-7
+                           border border-[#E5E7EB]
+                           rounded-full
+                           text-base text-body-color
+                           font-medium
+                           hover:border-primary hover:bg-primary hover:text-white
+                           transition
+                           "
+                           >
+                        Explore
+                        </a>
+                     </div>
+                  </div>
                </div>
-            </div>
-         </div>
-         {/* Card 2 */}
-         <div class="w-full md:w-1/2 xl:w-1/3 px-4">
-            <div class="bg-white rounded-lg overflow-hidden mb-10">
-               <img
-                  src="https://images.unsplash.com/photo-1530841377377-3ff06c0ca713?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHNhbnRvcmluaXxlbnwwfHwwfHx8MA%3D%3D"
-                  alt=""
-                  class="w-full"
-                  />
-               <div class="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
-                  <h3>
-                     <a
-                        href="javascript:void(0)"
-                        class="
-                        font-semibold
-                        text-dark text-xl
-                        sm:text-[22px]
-                        md:text-xl
-                        lg:text-[22px]
-                        xl:text-xl
-                        2xl:text-[22px]
-                        mb-4
-                        block
-                        hover:text-primary
-                        "
-                        >
-                     Santorini
-                     </a>
-                  </h3>
-                  <p class="text-base text-body-color leading-relaxed mb-7">
-                    Santorini is located in Greece and know for white washed buldings, along the coastline of Greece.
-                  </p>
-                  <a
-                     href="javascript:void(0)"
-                     class="
-                     inline-block
-                     py-2
-                     px-7
-                     border border-[#E5E7EB]
-                     rounded-full
-                     text-base text-body-color
-                     font-medium
-                     hover:border-primary hover:bg-primary hover:text-white
-                     transition
-                     "
-                     >
-                    Explore
-                  </a>
+               {/* Card 2 */}
+               <div class="w-full md:w-1/2 xl:w-1/3 px-4">
+                  <div class="bg-white rounded-lg overflow-hidden mb-10">
+                     <img
+                        src="https://images.unsplash.com/photo-1530841377377-3ff06c0ca713?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHNhbnRvcmluaXxlbnwwfHwwfHx8MA%3D%3D"
+                        alt=""
+                        class="w-full"
+                        />
+                     <div class="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
+                        <h3>
+                           <a
+                              href="javascript:void(0)"
+                              class="
+                              font-semibold
+                              text-dark text-xl
+                              sm:text-[22px]
+                              md:text-xl
+                              lg:text-[22px]
+                              xl:text-xl
+                              2xl:text-[22px]
+                              mb-4
+                              block
+                              hover:text-primary
+                              "
+                              >
+                           Santorini
+                           </a>
+                        </h3>
+                        {/* Card 2 Description */}
+                        <p class="text-base text-body-color leading-relaxed mb-7">
+                        Santorini is located in Greece and know for white washed buldings, along the coastline of Greece.
+                        </p>
+                        <a
+                           href="javascript:void(0)"
+                           class="
+                           inline-block
+                           py-2
+                           px-7
+                           border border-[#E5E7EB]
+                           rounded-full
+                           text-base text-body-color
+                           font-medium
+                           hover:border-primary hover:bg-primary hover:text-white
+                           transition
+                           "
+                           >
+                        Explore
+                        </a>
+                     </div>
+                  </div>
                </div>
-            </div>
-         </div>
-         {/* Card 3 */}
-         <div class="w-full md:w-1/2 xl:w-1/3 px-4">
-            <div class="bg-white rounded-lg overflow-hidden mb-10">
-               <img
-                  src="https://images.unsplash.com/photo-1612698093158-e07ac200d44e?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YW1hbGZpfGVufDB8fDB8fHww"
-                  alt=""
-                  class="w-full"
-                  loading='lazy'
-                  />
-               <div class="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
-                  <h3>
-                     <a
-                        href="javascript:void(0)"
-                        class="
-                        font-semibold
-                        text-dark text-xl
-                        sm:text-[22px]
-                        md:text-xl
-                        lg:text-[22px]
-                        xl:text-xl
-                        2xl:text-[22px]
-                        mb-4
-                        block
-                        hover:text-primary
-                        "
-                        >
-                     Amalfi
-                     </a>
-                  </h3>
-                  <p class="text-base text-body-color leading-relaxed mb-7">
-                     Located along the coastline of Amalif Coast in Italy. Know for picturesque views and locations.
-                  </p>
-                  <a
-                     href="#"
-                     class="
-                     inline-block
-                     py-2
-                     px-7
-                     border border-[#E5E7EB]
-                     rounded-full
-                     text-base text-body-color
-                     font-medium
-                     hover:border-primary hover:bg-primary hover:text-white
-                     transition
-                     "
-                     >
-                  Explore
-                  </a>
+               {/* Card 3 */}
+               <div class="w-full md:w-1/2 xl:w-1/3 px-4">
+                  <div class="bg-white rounded-lg overflow-hidden mb-10">
+                     <img
+                        src="https://images.unsplash.com/photo-1612698093158-e07ac200d44e?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YW1hbGZpfGVufDB8fDB8fHww"
+                        alt=""
+                        class="w-full"
+                        loading='lazy'
+                        />
+                     <div class="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
+                        <h3>
+                           <a
+                              href="javascript:void(0)"
+                              class="
+                              font-semibold
+                              text-dark text-xl
+                              sm:text-[22px]
+                              md:text-xl
+                              lg:text-[22px]
+                              xl:text-xl
+                              2xl:text-[22px]
+                              mb-4
+                              block
+                              hover:text-primary
+                              "
+                              >
+                           Amalfi
+                           </a>
+                        </h3>
+                        {/* Card 3 Description */}
+                        <p class="text-base text-body-color leading-relaxed mb-7">
+                           Located along the coastline of Amalif Coast in Italy. Know for picturesque views and locations.
+                        </p>
+                        <a
+                           href="#"
+                           class="
+                           inline-block
+                           py-2
+                           px-7
+                           border border-[#E5E7EB]
+                           rounded-full
+                           text-base text-body-color
+                           font-medium
+                           hover:border-primary hover:bg-primary hover:text-white
+                           transition
+                           "
+                           >
+                        Explore
+                        </a>
+                     </div>
+                  </div>
                </div>
-            </div>
+               {/* View More Button */}
+               <div class='container py-10 px-10 mx-0 min-w-full grid place-items-center'>
+                  <button>
+                        <a href="#" class="text-center text-sm px-4 py-2 leading-none border rounded text-black border-black hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">
+                           VIEW MORE TOURS
+                        </a>
+                  </button>
+               </div>
          </div>
-         <button>
-               <a href="#" class="text-center text-sm px-4 py-2 leading-none border rounded text-black border-black hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">
-                  VIEW MORE TOURS
-               </a>
-         </button>
-      </div>
    </div>
 </section>
 </div>
--- src/Hero/Search.js
+++ src/Hero/Search.js
@@ -1,7 +1,10 @@
+// Book Component
 import React from 'react';
+// React Icons
 import { MdCardTravel } from "react-icons/md";
 import { MdOutlineTravelExplore } from 'react-icons/md';
 
+// Main Component
 function Search() {
   return (
     <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3 gap-4 px-4 py-16">
@@ -19,6 +22,7 @@ function Search() {
             diving at most resorts. 
           </p>
         </div>
+        {/* Icons */}
         <div className="grid sm:grid-cols-2 gap-8 py-4">
           <div className="flex flex-col lg:flex-row items-center text-center">
             <button>
@@ -26,7 +30,7 @@ function Search() {
             </button>
             <div>
               <p className="py-1">
-                BEST TRAVEL COMPANY FOR 20 YEARS IN-A-ROW
+                BEST TRAVEL COMPANY FOR 20 YEARS
               </p>
             </div>
           </div>
@@ -42,11 +46,13 @@ function Search() {
           </div>
         </div>
       </div>
+      {/********************/}
       <div>
         <div className="border text-center">
           <p className="pt-2">GET ADDITIONAL 10% LEFT</p>
           <p className="bg-gray-800 text-gray-200 py-2">BOOK NOW AND SAVE</p>
         </div>
+        {/********************/}
         <form className="w-full">
           <div className="flex flex-col my-2">
             <label htmlFor="">Destinations</label>
@@ -60,6 +66,7 @@ function Search() {
               <option value="">Barbados</option>
             </select>
           </div>
+          {/* Check-in & Check-out */}
           <div className="flex flex-col my-2">
             <label>Check-in</label>
             <input
@@ -74,10 +81,11 @@ function Search() {
               type="date"
             />
           </div>
+          {/* Rates Button */}
           <div>
             <button className="w-full my-4 cursor-pointer">
-              <a href='' class="inline-block text-sm px-4 py-3 bg-blue-600 leading-none border rounded text-white hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">
-                Rates & Availabilities
+              <a href='' class="inline-block text-sm px-4 py-3 bg-blue-600 leading-none border rounded text-white hover:text-blue-500 hover:bg-white mt-4 lg:mt-0">
+                BOOK NOW
               </a>
             </button>
           </div>
