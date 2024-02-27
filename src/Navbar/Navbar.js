e37f1aed10ed9f5d7d20fe3b2da43c324f4f9569
el634dev <al7758066@gmail.com>
2024-02-27 14:48:54 -0500

    Almost done

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+// import { NavLink } from 'react-router-dom';
 // import Popup from 'reactjs-popup';
 
 function Navbar() {
@@ -24,27 +25,27 @@ function Navbar() {
                 {/* Navbar Items */}
                 <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
                     <div class="text-md lg:flex-grow text-center">
-                        <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                        <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
                             TOURS
                         </a>
                         {/******************/}
-                        <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                        <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
                             NEED TO KNOW
                         </a>
                         {/******************/}
-                        <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+                        <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
                             STORIES
                         </a>
                         {/******************/}
-                        <a href="#responsive-header" class="block mt-4 ml-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+                        <a href="#" class="block mt-4 ml-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
                            ABOUT
                         </a>
                     </div>
                     {/******************/}
-                    <div className="relative">
+                    <div>
                         <button
                             onClick={() => setIsOpen(!isOpen)}
-                            class="text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+                            className="text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
                         >
                             CONTACT
                         </button>
@@ -67,15 +68,6 @@ function Navbar() {
                         )}
                     </div>
                 </div>
-                {/******************/}
-                {/* <div class="flex" data-te-input-wrapper-init>
-                    <input
-                        type="search"
-                        class="block min-w-0  ml-3 max-w-[50%] flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
-                        placeholder="Search"
-                        aria-label="Search"
-                        aria-describedby="button-addon2" />
-                </div> */}
             </nav>
         </div>
     );
